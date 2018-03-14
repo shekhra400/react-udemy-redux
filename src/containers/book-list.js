fcdd469730f659aeb73e119d3c42684f33ea96b3
shekhar <sshukla17@sapient.com>
2018-03-14 18:44:22 +0530

    first commit

---
+++ src/containers/book-list.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class BookList extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    renderList(){
+        return this.props.books.map((book) => {
+            return (
+                <li className="list-group-item" key={book.name}>{book.name}</li>
+            );
+        });
+    }
+
+    render(){
+        return(
+            <ul className="col-md-4 list-group">
+                {this.renderList()}
+            </ul>
+        )
+    }
+
+}
+
+function mapStateToProps(state){
+    return {
+        books: state.books
+    }
+}
+
+export default connect(mapStateToProps)(BookList);
